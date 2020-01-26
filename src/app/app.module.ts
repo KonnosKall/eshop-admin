17a12341dec818fbc22a670da67549481bffdaff
Konstantinos Kallipolitis <konnoskall@gmail.com>
2020-01-27 00:37:17 +0200

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {ProductCreateComponent} from './components/product-create/product-create
 import {ProductUpdateComponent} from './components/product-update/product-update.component';
 import {CategoriesComponent} from './components/categories/categories.component';
 import { EditorModule } from '@tinymce/tinymce-angular';
+import { ClientsComponent } from './components/clients/clients.component';
 
 const routes = [
   {
@@ -56,7 +57,8 @@ const routes = [
     ProductsComponent,
     ProductCreateComponent,
     ProductUpdateComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule,
